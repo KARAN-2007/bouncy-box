45ae209f7cff0132933bd2f7a971d32038af835c
KARAN-2007 <76608112+KARAN-2007@users.noreply.github.com>
2021-01-22 13:17:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var box,b1,b2,b3,b4,edges;
+function setup(){
+  createCanvas(500,500);
+  box=createSprite(Math.round(random(20,490)),Math.round(random(20,450)),15,15);
+  
+  box.velocityX=2;
+  box.velocityY=5;
+  b1=createSprite(437.5,492,125,15);
+  b1.shapeColor="green";
+  b2=createSprite(312.5,492,125,15);
+  b2.shapeColor="blue";
+  b3=createSprite(187.5,492,125,15);
+  b3.shapeColor="red";
+  b4=createSprite(62.5,492,125,15);
+  b4.shapeColor="orange";
+  edges=createEdgeSprites();
+  
+}
+function draw(){
+  background("black");
+ // box.y=mouseY;
+ // box.x=mouseX;
+  
+  
+  
+  if(isTouching(box,b1)&& bounceOff(box,b1)){
+    
+    box.shapeColor=b1.shapeColor;
+    
+  }
+  if(isTouching(box,b2)){
+   
+    box.shapeColor=b2.shapeColor;
+  
+  }
+  if(isTouching(box,b3)){
+   
+    box.shapeColor=b3.shapeColor;
+    
+  }
+  if(isTouching(box,b4)){
+   
+    box.shapeColor=b4.shapeColor;
+    box.velocityX=0;
+    box.velocityY=0;
+  }
+  
+  
+  
+  bounceOff(box,b2);
+  bounceOff(box,b3);
+  
+  drawSprites()
+}
+
